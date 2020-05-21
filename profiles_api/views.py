8600e1913b69c23223d3e73d39c610b1858d5f6a
mallaiya <mallaiyazap@gmail.com>
2020-05-21 13:27:53 +0530

    API Test View | Added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,59 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+class TestAPIView(APIView):
+    serializer_data = serializers.TestSerializer
+
+    def get(self, request, format=None):
+        data = [
+            {
+                'name': 'Mallaiya',
+                'age': 20,
+            },
+            {
+                'name': 'Tamil',
+                'age': 20,
+            }
+        ]
+        return Response({'message': 'Successfully got data', 'data': data})
+
+
+    def post(self, request):
+        serializer = self.serializer_data(data=request.data)
+
+        if(serializer.is_valid()):
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        serializer = self.serializer_data(data=request.data)
+
+        if (serializer.is_valid()):
+            name = serializer.validated_data.get('name')
+            message = f'{name} updated'
+
+            return Response({'message': message})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def patch(self, request, pk=None):
+        serializer = self.serializer_data(data=request.data)
+
+        if (serializer.is_valid()):
+            name = serializer.validated_data.get('name')
+            message = f'{name} patched successfully'
+
+            return Response({'message': message})
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk=None):
+        return Response({'message':  'Deleted Successfully'})
